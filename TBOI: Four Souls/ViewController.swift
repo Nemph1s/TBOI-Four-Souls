da5f28f6110dc91381dc3ac4ccc970fa91ba8d3f
VMartyniuk <inemphis@gmail.com>
2018-09-08 18:25:17 +0300

    Initial Commit

---
+++ TBOI: Four Souls/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TBOI: Four Souls
+//
+//  Created by  Volodymyr Martyniuk on 9/8/18.
+//  Copyright © 2018 VMart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TBOI: Four SoulsTests/TBOI__Four_SoulsTests.swift
@@ -0,0 +1,34 @@
+//
+//  TBOI__Four_SoulsTests.swift
+//  TBOI: Four SoulsTests
+//
+//  Created by  Volodymyr Martyniuk on 9/8/18.
+//  Copyright © 2018 VMart. All rights reserved.
+//
+
+import XCTest
+@testable import TBOI__Four_Souls
+
+class TBOI__Four_SoulsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
