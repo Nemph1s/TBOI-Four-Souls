bccb79f1afccb6d64a5a2d370f0b0ec11df8b9b5
VMartyniuk <inemphis@gmail.com>
2018-09-09 01:18:52 +0300

    first commit

---
+++ TBOI: Four Souls/Model/CardInfo.swift
@@ -0,0 +1,9 @@
+//
+//  CardInfo.swift
+//  TBOI: Four Souls
+//
+//  Created by  Volodymyr Martyniuk on 9/8/18.
+//  Copyright © 2018 VMart. All rights reserved.
+//
+
+import Foundation
---
+++ TBOI: Four Souls/View/CustomCollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionCell.swift
+//  TBOI: Four Souls
+//
+//  Created by  Volodymyr Martyniuk on 9/9/18.
+//  Copyright © 2018 VMart. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionCell: UICollectionViewCell {
+    
+}
